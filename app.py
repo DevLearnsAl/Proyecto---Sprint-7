6fa864db748d70433dae56c9d7076efce90e2a82
Daniel Alaniz <alberto.alaniz1139@gmail.com>
2026-01-07 09:49:40 -0600

    Created 2 check boxes to display scatter and histogram plots.

--- app.py
+++ app.py
@@ -9,19 +9,25 @@ car_data = pd.read_csv('vehicles_us.csv')
 # Creamos un encabezado para la aplicación
 st.header("Gráfico de Dispersión de Precio vs. Odómetro")
 
-# Creamos un botón que construye un gráfico histograma al ser presionado.
-hist_button = st.button('Construir histograma')
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
 
-if hist_button:
+# Despleagamos el histograma si la casilla está seleccionada
+if build_histogram:
     st.write(
-        "Creación de un histograma para el conjunto de datos de de anuncios de venta de carros")
+        "Construir un histograma para la columna odómetro")
 
     fig = px.histogram(car_data, x='odometer')
 
     st.plotly_chart(fig, use_container_width=True)
 
-# crear una casilla de verificación
-build_histogram = st.checkbox('Construir un histograma')
+# Crear una casilla de verificación para el gráfico de dispersión
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
 
-if build_histogram:  # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
+if build_scatter:  # si la casilla de verificación está seleccionada
+    st.write(
+        "Construir un gráfico de dispersión para las columnas odómetro y precio")
+# Construimos el gráfico de dispersión
+    fig = px.scatter(car_data, x='odometer', y='price')
+
+    st.plotly_chart(fig, use_container_width=True)
